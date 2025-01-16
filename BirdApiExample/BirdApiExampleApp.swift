9a21900588454a834f379edb68a6e4f7f6bc6b4b
Reed Gantz <reedm121@gmail.com>
2025-01-16 13:21:21 -0500

    Initial Commit

---
+++ BirdApiExample/BirdApiExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdApiExampleApp.swift
+//  BirdApiExample
+//
+//  Created by Reed Gantz on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct BirdApiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
